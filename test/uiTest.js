0a15264d69c70cdb6ca6a636222ddc2f9b67d540
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-07-14 18:10:31 -0300

    removing changed files

--- test/uiTest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  'Test title': function (test) {
-    test
-      .open('http://localhost:3000')
-      .assert.title().is('New Site', 'it has title')
-      .done();
-  }
-};
